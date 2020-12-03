ca1b00dd8fa10a074986847a5c32e9a639069e53
Samuel Duval <samuel.k.duval@gmail.com>
2020-12-03 15:09:35 +0100

    Added namespaces

--- Assets/Editor/UnityPackageExporter.cs
+++ Assets/Editor/UnityPackageExporter.cs
@@ -2,29 +2,32 @@
 using UnityEditor;
 using UnityEngine;
 
-public static class UnityPackageExporter
+namespace ARWT.Core
 {
-    private static readonly string PACKAGE_FILE_NAME = Path.Combine(
-        path1: Application.dataPath,
-        path2: "ARWT.unitypackage"
-    );
-
-    private static readonly string[] ASSET_PATH_NAMES = new string[]
+    public static class UnityPackageExporter
     {
+        private static readonly string PACKAGE_FILE_NAME = Path.Combine(
+            path1: Application.dataPath,
+            path2: "ARWT.unitypackage"
+        );
+
+        private static readonly string[] ASSET_PATH_NAMES = new string[]
+        {
         "Assets/WebGLTemplates",
         "Assets/Resources/Prefabs",
         "Assets/Plugins",
         "Assets/Scripts"
-    };
+        };
 
-    [MenuItem("ARWT/Export Unity Package")]
-    public static void ExportPackage()
-    {
-        AssetDatabase.ExportPackage(
-            assetPathNames: ASSET_PATH_NAMES,
-            fileName: PACKAGE_FILE_NAME,
-            ExportPackageOptions.Recurse | ExportPackageOptions.Interactive
-        );
-        AssetDatabase.Refresh();
+        [MenuItem("ARWT/Export Unity Package")]
+        public static void ExportPackage()
+        {
+            AssetDatabase.ExportPackage(
+                assetPathNames: ASSET_PATH_NAMES,
+                fileName: PACKAGE_FILE_NAME,
+                ExportPackageOptions.Recurse | ExportPackageOptions.Interactive
+            );
+            AssetDatabase.Refresh();
+        }
     }
 }
