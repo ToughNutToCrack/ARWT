ca9ceb5e82f746cb5dda7c3bb7737ea01133b530
pandakub <coxjart@gmail.com>
2021-06-17 09:38:52 -0400

    Added Input Controls

---
+++ Assets/App/Internal/Scripts/InputController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public static InputController Instance;
+
+    [SerializeField]
+    PlayerController _playerController;
+    
+    public enum PointerCommands
+    {
+        None,
+        MoveForward,
+        MoveBackward,
+        RotateRight,
+        RotateLeft,
+        Shoot,
+    }
+
+    Dictionary<PointerCommands, System.Action<bool>> _pointerEvents;
+
+    public static void ProcessInput(PointerCommands pointerCommand, bool pointerDown)
+    {
+        if(Instance != null)
+        {
+            var events = Instance._pointerEvents;
+            if(events.TryGetValue(pointerCommand, out System.Action<bool> e))
+            {
+                e.Invoke(pointerDown);
+            }
+        }
+    }
+
+    void Awake()
+    {
+        Instance = this;
+
+        _pointerEvents = _pointerEvents ?? new Dictionary<PointerCommands, System.Action<bool>>()
+        {
+            { PointerCommands.MoveForward, OnMoveForward },
+            { PointerCommands.MoveBackward, OnMoveBackward },
+            { PointerCommands.RotateLeft, OnRotateLeft },
+            { PointerCommands.RotateRight, OnRotateRight },
+            { PointerCommands.Shoot, OnShoot },
+        };
+    }
+
+    void OnMoveForward(bool pointerDown) 
+    {
+        if(_playerController != null)
+        {
+            _playerController.IsMovingForward = pointerDown;
+        }
+    }
+
+    void OnMoveBackward(bool pointerDown)
+    {
+        if (_playerController != null)
+        {
+            _playerController.IsMovingBackward = pointerDown;
+        }
+    }
+
+    void OnRotateRight(bool pointerDown)
+    {
+        if (_playerController != null)
+        {
+            _playerController.IsRotatingRight = pointerDown;
+        }
+    }
+
+    void OnRotateLeft(bool pointerDown)
+    {
+        if (_playerController != null)
+        {
+            _playerController.IsRotatingLeft = pointerDown;
+        }
+    }
+
+    void OnShoot(bool pointerDown)
+    {
+        if (_playerController != null)
+        {
+            _playerController.ShootGun();
+        }
+    }
+}
---
+++ Assets/App/Internal/Scripts/InputControllerTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+public class InputControllerTrigger : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField]
+    InputController.PointerCommands Command;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if(Command != InputController.PointerCommands.None)
+        {
+            InputController.ProcessInput(Command, true);
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (Command != InputController.PointerCommands.None)
+        {
+            InputController.ProcessInput(Command, false);
+        }
+    }
+}
